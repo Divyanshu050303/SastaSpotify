8c785c9344a168f7ca2adc8e5b9aa1c073945115
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-07-03 20:52:25 +0530

    download screen

---
+++ app/src/main/java/com/example/sastaspotify/download.kt
@@ -0,0 +1,12 @@
+package com.example.sastaspotify
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun download(modifier: Modifier = Modifier){
+    Text("abhay", modifier = Modifier.padding(24.dp))
+}
