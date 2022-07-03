dadaa5284684d59c83cf1b02967651e6615c745e
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-07-03 20:52:07 +0530

    color

---
+++ app/src/main/java/com/example/sastaspotify/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.sastaspotify.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFF03DAC5)
+val Purple500 = Color(0xFFFFFFFF)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val pink = Color(0xFFF1256A)
