35c406e3e6ca284ae0c111230127b690833dcf7b
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-07-03 20:53:12 +0530

    Favorite screen

---
+++ app/src/main/java/com/example/sastaspotify/fav.kt
@@ -0,0 +1,15 @@
+package com.example.sastaspotify
+
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+
+@Composable
+fun Favorite(modifier: Modifier= Modifier){
+    Text("Chinu", modifier = Modifier.padding(24.dp))
+}
