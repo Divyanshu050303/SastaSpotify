d51c6439c45367572eaaf01845ac80fc15a8c2ba
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-07-03 20:54:32 +0530

    setting screen

---
+++ app/src/main/java/com/example/sastaspotify/setting.kt
@@ -0,0 +1,12 @@
+package com.example.sastaspotify
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Setting(modifier: Modifier = Modifier){
+    Text("anurag", modifier = Modifier.padding(24.dp))
+}
