e73fff0964c44d3f7f3c5893c2acdb7c0f2da4b2
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-07-04 16:55:08 +0530

    setting screen

--- app/src/main/java/com/example/sastaspotify/setting.kt
+++ app/src/main/java/com/example/sastaspotify/setting.kt
@@ -1,12 +1,33 @@
 package com.example.sastaspotify
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Setting(modifier: Modifier = Modifier){
-    Text("anurag", modifier = Modifier.padding(24.dp))
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Setting Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
 }
