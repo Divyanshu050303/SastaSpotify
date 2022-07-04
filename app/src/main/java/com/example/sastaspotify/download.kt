9e18ce8de1c1a4b932ce8e03c8c7e6280b20c09b
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-07-04 16:52:49 +0530

    download screen

--- app/src/main/java/com/example/sastaspotify/download.kt
+++ app/src/main/java/com/example/sastaspotify/download.kt
@@ -1,12 +1,34 @@
 package com.example.sastaspotify
 
-import androidx.compose.foundation.layout.padding
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun download(modifier: Modifier = Modifier){
-    Text("abhay", modifier = Modifier.padding(24.dp))
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = " Download Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
 }
