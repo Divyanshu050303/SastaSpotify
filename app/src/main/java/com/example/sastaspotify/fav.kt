4126800b1315a3394369706ba72fe4e0c3202bbe
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-07-04 16:53:06 +0530

    Favorite screen

--- app/src/main/java/com/example/sastaspotify/fav.kt
+++ app/src/main/java/com/example/sastaspotify/fav.kt
@@ -1,15 +1,35 @@
 package com.example.sastaspotify
 
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun Favorite(modifier: Modifier= Modifier){
-    Text("Chinu", modifier = Modifier.padding(24.dp))
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Favorite Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
 }
