de768c102bdf6065f0489b03715451f9320353be
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-07-08 17:36:45 +0530

    change the size

--- app/src/main/java/com/example/sastaspotify/BottomNavItem.kt
+++ app/src/main/java/com/example/sastaspotify/BottomNavItem.kt
@@ -1,7 +1,4 @@
 package com.example.sastaspotify
-
-import androidx.compose.material.icons.Icons
-
 sealed class BottomNavItem(var title:String, var icon:Int,var screen_route:String ) {
     object Home:BottomNavItem("Home",R.drawable.home, "home")
     object Favorite:BottomNavItem("Favorite",R.drawable.favorite, "favorite")
