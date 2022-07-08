62286c546035f4df9a77121c15a9c2d695e25cbf
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-07-08 17:37:03 +0530

    add card

--- app/src/main/java/com/example/sastaspotify/setting.kt
+++ app/src/main/java/com/example/sastaspotify/setting.kt
@@ -1,33 +1,62 @@
 package com.example.sastaspotify
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun Setting(modifier: Modifier = Modifier){
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp, vertical = 10.dp)
     ) {
-        Text(
-            text = "Setting Screen",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
+        val paddingModifier=Modifier.padding(10.dp);
+        Text("Settings", modifier = paddingModifier, style = MaterialTheme.typography.h4)
+
+        Card(
+            border = BorderStroke(2.dp, Color.Red),
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .size(70.dp)) {
+            Row(modifier = Modifier.padding(horizontal = 4.dp, vertical = 11.dp)) {
+                Icon(painter = painterResource(id = R.drawable.person), contentDescription = null, modifier = Modifier.padding(vertical = 6.dp))
+                Text(text = "Profile", modifier = paddingModifier)
+            }
+        }
+        Card(
+            border = BorderStroke(2.dp, Color.Red),
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .size(70.dp)) {
+            Row(modifier = Modifier.padding(horizontal = 4.dp, vertical = 11.dp)) {
+                Icon(painter = painterResource(id = R.drawable.language), contentDescription = null, modifier = Modifier.padding(vertical = 6.dp))
+                Text(text = "Language", modifier = paddingModifier)
+            }
+        }
+        Card(
+            border = BorderStroke(2.dp, Color.Red),
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .size(70.dp)) {
+            Row(modifier = Modifier.padding(horizontal = 4.dp, vertical = 11.dp)) {
+                Icon(painter = painterResource(id = R.drawable.about), contentDescription = null, modifier = Modifier.padding(vertical = 6.dp))
+                Text(text = "About", modifier = paddingModifier)
+            }
+        }
     }
 }
