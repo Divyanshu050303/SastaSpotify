28f424b1bb7b31b634d8de46db4bf7e955709efc
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-07-21 16:49:31 +0530

    Setting screen

--- app/src/main/java/com/example/sastaspotify/setting.kt
+++ app/src/main/java/com/example/sastaspotify/setting.kt
@@ -3,21 +3,18 @@ package com.example.sastaspotify
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun Setting(modifier: Modifier = Modifier){
+fun Setting(modifier: Modifier = Modifier, navController: NavController){
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -31,9 +28,9 @@ fun Setting(modifier: Modifier = Modifier){
             modifier = Modifier
                 .padding(10.dp)
                 .fillMaxWidth()
-                .size(70.dp)) {
+                .size(70.dp).clickable(onClick = { navController.navigate(Screen.Profile.toString()) })) {
             Row(modifier = Modifier.padding(horizontal = 4.dp, vertical = 11.dp)) {
-                Icon(painter = painterResource(id = R.drawable.person), contentDescription = null, modifier = Modifier.padding(vertical = 6.dp))
+                Icon(painter = painterResource(id = R.drawable.person1), contentDescription = null, modifier = Modifier.padding(vertical = 6.dp))
                 Text(text = "Profile", modifier = paddingModifier)
             }
         }
@@ -54,7 +51,8 @@ fun Setting(modifier: Modifier = Modifier){
             modifier = Modifier
                 .padding(10.dp)
                 .fillMaxWidth()
-                .size(70.dp)) {
+                .size(70.dp)
+                .clickable (onClick = { navController.navigate(Screen.Quality.toString())  })) {
             Row(modifier = Modifier.padding(horizontal = 4.dp, vertical = 11.dp)) {
                 Icon(painter = painterResource(id = R.drawable.audio), contentDescription = null, modifier = Modifier.padding(vertical = 6.dp))
                 Text(text = "Audio Quality", modifier = paddingModifier)
