52c68cc2578d986243a239c8fffa68d5e017206d
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-08-04 17:25:10 +0530

    Data Provide

--- app/src/main/java/com/example/sastaspotify/data/dataProvider.kt
+++ app/src/main/java/com/example/sastaspotify/data/dataProvider.kt
@@ -2,90 +2,136 @@ package com.example.sastaspotify.data
 
 
 import com.example.sastaspotify.R
-import com.example.sastaspotify.data.model.SingerPlaylist
 
 
-object dataProvider {
-    val Singer= SingerPlaylist(
-        id=1,
-        name="Alka Yagnik",
-        Image= R.drawable.alka_yagnik
-    )
+data class DataProvider(val name: String ,val Image:Int) {
+    val singerName:String
+    get()=name
+    val singerImage:Int
+    get()=Image
+
+}
+
     val SingerList= listOf(
-        Singer,
-        SingerPlaylist(
-            id=2,
+        DataProvider(
+            name="Alka Yagnik",
+            Image= R.drawable.alka_yagnik),
+        DataProvider(
             name="AR Rahman",
             Image=R.drawable.ar_rahman
-        ), SingerPlaylist(
-            id=3,
+        ),
+        DataProvider(
             name="Amit Trivedi",
             Image=R.drawable.amitt_trivedi
-        ), SingerPlaylist(
-            id=4,
+        ),
+        DataProvider(
             name="Arjit Singh",
             Image=R.drawable.arjit_singh
-        ), SingerPlaylist(
-            id=5,
+        ),
+        DataProvider(
             name="Asha Bhonsle",
             Image=R.drawable.asha_bhonsle
-        ), SingerPlaylist(
-            id=6,
+        ),
+        DataProvider(
             name="Badshah",
             Image=R.drawable.badshah
-        ), SingerPlaylist(
-            id=7,
+        ),
+        DataProvider(
             name="Honey Singh",
             Image=R.drawable.honey_singh
-        ), SingerPlaylist(
-            id=8,
+        ),
+        DataProvider(
             name="Kanika Kapoor",
             Image=R.drawable.kanika
-        ), SingerPlaylist(
-            id=9,
+        ),
+        DataProvider(
             name="Neha Kakkar",
             Image=R.drawable.neha_kakkar
-        ), SingerPlaylist(
-            id=10,
+        ),
+        DataProvider(
             name="Kumar Sanu",
             Image=R.drawable.kumar_sanu
-        ), SingerPlaylist(
-            id=11,
+        ),
+        DataProvider(
             name="Monali Thakur",
             Image=R.drawable.monali_thakur
-        ), SingerPlaylist(
-            id=12,
+        ),
+        DataProvider(
             name="Palak Muchhal",
             Image=R.drawable.palak_muchhal
-        ), SingerPlaylist(
-            id=13,
+        ),
+        DataProvider(
             name="Shreya Ghoshal",
             Image=R.drawable.shreya_ghoshal
-        ), SingerPlaylist(
-            id=14,
+        ),
+        DataProvider(
             name="Sonu Nigam",
             Image=R.drawable.sonu_nigam
-        ), SingerPlaylist(
-            id=15,
+        ),
+        DataProvider(
             name="Sonu Kakkar",
             Image=R.drawable.sonu
         ),
-        SingerPlaylist(
-            id=16,
+        DataProvider(
             name="Tony kakkar",
             Image=R.drawable.tony_kakkar
-        ),SingerPlaylist(
-            id=17,
+        ),
+        DataProvider(
             name="Tulsi Kumar",
             Image=R.drawable.tulsi_kumar
-        ), SingerPlaylist(
-            id=18,
+        ),
+        DataProvider(
             name="Udit Narayan",
             Image=R.drawable.udit_narayan
-        ), SingerPlaylist(
-            id=19,
+        ),
+        DataProvider(
             name="Guru Randhawa",
             Image=R.drawable.guru_randhawa
-        ),
+        )
     )
-}
+val popularAlbums= listOf(
+    DataProvider(
+    name = "Hit Hindi",
+    Image = R.drawable.hindi),
+    DataProvider(
+        name = "Hit Punjabi",
+        Image = R.drawable.punjabi),
+    DataProvider(
+        name = "Top Global",
+        Image = R.drawable.topglobal),
+    DataProvider(
+        name = "Hit Tamil",
+        Image = R.drawable.tamil),
+    DataProvider(
+        name = "Hit Telugu",
+        Image = R.drawable.telugu),
+    DataProvider(
+        name = "Viral Global",
+        Image = R.drawable.viralglobal),
+    DataProvider(
+        name = "Top India",
+        Image = R.drawable.top50india)
+)
+val MoodAlbums= listOf(
+    DataProvider(
+    name = "Mood Booster",
+    Image = R.drawable. myself),
+    DataProvider(
+        name = "Feeling'Good",
+        Image = R.drawable.feelingood),
+    DataProvider(
+        name = "Dark & Stromy",
+        Image = R.drawable.dark),
+    DataProvider(
+        name = "Feel Good Piano",
+        Image = R.drawable.feelgoodpiano),
+    DataProvider(
+        name = "Feelin'MySelf",
+        Image = R.drawable.myself),
+    DataProvider(
+        name = "You & me",
+        Image = R.drawable.youandme),
+    DataProvider(
+        name = "Calm Before Storm",
+        Image = R.drawable.calmdeforestrome)
+)
