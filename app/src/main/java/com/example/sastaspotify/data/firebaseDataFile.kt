90cf372c39d909af4020375cec7ec0cf04cb873c
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-08-17 20:49:58 +0530

    firebaseDataFile

--- app/src/main/java/com/example/sastaspotify/data/firebaseDataFile.kt
+++ app/src/main/java/com/example/sastaspotify/data/firebaseDataFile.kt
@@ -1,9 +1,9 @@
 package com.example.sastaspotify.data
 
 data class firebaseDataFile(
-    var id:Int?=null,
+    var id:String?=null,
     val name:String?=null,
-    var Image:Int?=null,
+    var Image:String?=null,
     var audio:String?=null
 )
 
