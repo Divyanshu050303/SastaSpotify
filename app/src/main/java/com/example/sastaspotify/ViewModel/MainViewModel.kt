8c88da576ba3cc5689b33f6e49721d60e989566d
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-08-25 20:44:14 +0530

    Fetching  data from firebase

--- app/src/main/java/com/example/sastaspotify/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/example/sastaspotify/ViewModel/MainViewModel.kt
@@ -1,38 +1,51 @@
 package com.example.sastaspotify.ViewModel
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.example.sastaspotify.data.DataState
 import com.example.sastaspotify.data.firebaseDataFile
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-class MainViewModel : ViewModel() {
-    val response:MutableState<DataState> = mutableStateOf(DataState.Empty)
+class MainViewModel : ViewModel()  {
+//    val response: MutableState<DataState> = mutableStateOf(DataState.Empty)
+    val temList= mutableListOf<firebaseDataFile>()
     init {
         FetchDataFromDatabase()
     }
     private fun FetchDataFromDatabase(){
-        val temList= mutableListOf<firebaseDataFile>()
-        response.value=DataState.Loading
+
+//        response.value=DataState.Loading
         FirebaseDatabase.getInstance().getReference("Alka Yagnik")
-            .addListenerForSingleValueEvent(object :ValueEventListener{
+            .addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot){
-                    for(DataSnap in snapshot.children){
-                        val Song=DataSnap.getValue(firebaseDataFile::class.java)
-                        if(Song!=null){
+                    for(DataSnap in snapshot.children) {
+                        val Song = DataSnap.getValue(firebaseDataFile::class.java)
+//                        if (Song != null) {
+                        if (Song != null) {
                             temList.add(Song)
                         }
+//                        }
                     }
                 }
 
+
                 override fun onCancelled(error: DatabaseError) {
-                     response.value=DataState.Failure(error.message)
+                    Log.w(TAG, "loadPost:onCancelled")
                 }
             }
             )
     }
+//    private val firebaseStorage=FirebaseStorage.getInstance()
+//    private  val SingerSongCollection=firebaseStorage.getReference("Alka Yagnik")
+//    suspend fun getAllString():List<firebaseDataFile>{
+//        return try {
+//            SingerSongCollection.
+//        }
+//        catch (e:Exception){
+//            emptyList()
+//        }
+//    }
 }
