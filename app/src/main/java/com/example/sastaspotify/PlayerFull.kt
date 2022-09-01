58fa0a9265fa531fd0d301a03d172b8d34669726
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-09-01 08:29:15 +0530

    Add player setting

--- app/src/main/java/com/example/sastaspotify/PlayerFull.kt
+++ app/src/main/java/com/example/sastaspotify/PlayerFull.kt
@@ -1,5 +1,8 @@
 package com.example.sastaspotify
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -9,10 +12,27 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import com.example.sastaspotify.data.firebaseDataFile
+
 
 @Composable
 fun Player (songDetail: firebaseDataFile, navController: NavController){
+    val url ="https://firebasestorage.googleapis.com/v0/b/sastaspotify-15dbe.appspot.com/o/Alka%20Yagnik%2FAisi%20Deewangi.mp3?alt=media&token=d9fdc6ff-5458-4bf5-90b3-050905c821ce"
+
+    val mediaPlayer = MediaPlayer().apply {
+        setAudioAttributes(
+            AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build()
+        )
+        setDataSource(url)
+        prepare()
+        start()
+
+    }
+
     Column(modifier = Modifier
         .padding(horizontal = 10.dp, vertical = 20.dp)
         .fillMaxWidth()) {
@@ -20,8 +40,9 @@ fun Player (songDetail: firebaseDataFile, navController: NavController){
             modifier = Modifier
                 .padding(10.dp)
                 .fillMaxWidth()
-                .size(400.dp)) {
-            Image(painter = painterResource(id = R.drawable.img), contentDescription =null, modifier = Modifier.fillMaxWidth() )
+                .width(400.dp)
+                .height(350.dp)) {
+            Image(painter = rememberAsyncImagePainter(songDetail.imageurl), contentDescription =null, modifier = Modifier.fillMaxWidth() )
 
         }
         Spacer(modifier = Modifier.height(5.dp))
@@ -44,7 +65,16 @@ fun Player (songDetail: firebaseDataFile, navController: NavController){
         Row (modifier = Modifier.fillMaxWidth()){
             Image(painter = painterResource(id = R.drawable.rewind), contentDescription =null , modifier = Modifier.padding(horizontal = 16.dp))
             Image(painter = painterResource(id = R.drawable.pernext), contentDescription =null, modifier = Modifier.padding(horizontal = 16.dp) )
-            Image(painter = painterResource(id = R.drawable.play), contentDescription =null , modifier = Modifier.padding(horizontal = 16.dp))
+            Image(painter = painterResource(id = if (mediaPlayer.isPlaying){R.drawable.pause} else{ R.drawable.play}),
+                contentDescription =null ,
+                modifier = Modifier.padding(horizontal = 16.dp)
+                    .clickable(onClick = {
+                        if(mediaPlayer.isPlaying){
+                            mediaPlayer.pause()
+                        }else {
+                            mediaPlayer.start()
+                        }
+                    }))
             Image(painter = painterResource(id = R.drawable.next), contentDescription =null, modifier = Modifier.padding(horizontal = 16.dp) )
             Image(painter = painterResource(id = R.drawable.fastforward), contentDescription =null , modifier = Modifier.padding(horizontal = 16.dp))
         }
