42c569a58054fd7875787b30c0331fffa9fcf88f
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-10-17 22:45:14 +0530

    firebase Data File

--- app/src/main/java/com/example/sastaspotify/data/firebaseDataFile.kt
+++ app/src/main/java/com/example/sastaspotify/data/firebaseDataFile.kt
@@ -1,13 +1,9 @@
 package com.example.sastaspotify.data
 
-import com.google.firebase.database.IgnoreExtraProperties
-import java.io.Serializable
-
-@IgnoreExtraProperties
 data class firebaseDataFile(
-    val imageurl:String="",
+    val image:String="",
     val name:String="",
-    val songurl:String=""
-): Serializable
+    val url:String=""
+)
 
 
