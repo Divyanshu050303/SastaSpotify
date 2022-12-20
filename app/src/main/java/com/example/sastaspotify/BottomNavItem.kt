51d48558b469953bc54da1800cce3bd3565cf72e
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-12-20 19:02:06 +0530

    Bottom Nav Item

--- app/src/main/java/com/example/sastaspotify/BottomNavItem.kt
+++ app/src/main/java/com/example/sastaspotify/BottomNavItem.kt
@@ -5,7 +5,6 @@ sealed class BottomNavItem(var title:String, var icon:Int,var screen_route:Strin
     object Playlist:BottomNavItem("Playlist",R.drawable.queue, "playlist")
     object Download:BottomNavItem("Download",R.drawable.dowmload1, "download")
     object Setting:BottomNavItem("Setting",R.drawable.settings, "setting")
-
 }
 sealed class Screen(){
     object Quality: Screen()
@@ -15,10 +14,4 @@ sealed class Screen(){
     object About:Screen()
     object  Home:Screen()
     object PlayerFull:Screen()
-}
-sealed class DetailScreen(val name :String, val image:Int, var route:String ){
-    object  Alka:DetailScreen("Alka Yagnik", R.drawable.alka_yagnik, "Alka Yagnik")
-    object  screen:DetailScreen("Honey Singh", R.drawable.honey_singh, "Honey Singh")
-//    object  Arjit:DetailScreen("Arjit Singh", R.drawable.arjit_singh)
-
 }
